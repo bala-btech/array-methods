afa5d3c1a3dec4c890c0993c3b02cfac74611371
bala-btech <lbalamurugan002@gmail.com>
2026-06-10 05:22:09 +0530

    first commit

--- script.js
+++ script.js
@@ -1,89 +1,52 @@
-// // Even or odd
-// const oe = (num) => {
-//     if (num % 2 == 0) {
-//         return "Even";
-//     } else {
-//         return "Odd";
-//     }
+// 1.looping through an array
+// const num = [10, 20, 30, 40];
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
 // }
 
-// console.log(oe(3));
+//for Each
+// let num = [10, 20, 30, 40];
+// num.forEach(num => {
+//   console.log(num);
+// });
 
-//Result evlauator
-// const findMax = (num1, num2, num3) => {
-//     if (num1 >= num2 && num1 >= num3) {
-//         return num1;
-//     } else if (num2 >= num1 && num2 >= num3) {
-//         return num2;
-//     } else {
-//         return num3;
-//     }
+//for of
+// const num = [10, 20, 30, 40];
+// for (const i of num) {
+//   console.log(i);
 // }
 
-// console.log(findMax(10, 25, 15));
-
-//accumulator
-// function sumToN(n) {
-//     let total = 0;
-//     for (let i = 1; i <= n; i++) {
-//         total = total + i;
-//     }
-//     return total;
+// for in
+// const num = [10, 20, 30, 40];
+// for (const i in num) {
+//   console.log(num[i]);
 // }
 
-//multiplication table
-// function printTable(num) {
-//     for (let i = 1; i <= 10; i++) {
-//         console.log(num + " x " + i + " = " + (num * i));
-//     }
+// Loop Through an Object
+
+// const student = {
+//   name: "Bala",
+//   age: 21,
+//   grade: "A"
+// }; 
+
+// for (const i in student) {
+//   console.log(i,student[i]);
 // }
 
-//digit counter
-// function countDigits(num) {
-//     if (num === 0) {
-//         return 1;
-//     }
-//     let count = 0;
-//     while (num > 0) {
-//         count++;
-//         num = Math.floor(num / 10);
-//     }
-//     return count;
-// }
+// using map()
+// const Marks = [50, 60, 70, 80];
+// const reducedMarks = Marks.map(i => i - 10);
+// console.log(reducedMarks); 
 
-//num reverser
-// function reverseNumber(num) {
-//     let reversed = 0;
-//     while (num > 0) {
-//         let lastDigit = num % 10;
-//         reversed = (reversed * 10) + lastDigit;
-//         num = Math.floor(num / 10);
-//     }
-//     return reversed;
-// }
 
-//Factorial
-// function factorial(num) {
-//     let result = 1;
-//     for (let i = 1; i <= num; i++) {
-//         result = result * i;
-//     }
-//     return result;
-// }
+//Using filter()
+// const values = [5, 12, 8, 25, 3, 15];
+// const val = values.filter(num => num > 10);
+// console.log(val); 
 
-// console.log(factorial(5));
 
-//prime validator
-// function isPrime(num) {
-//     if (num < 2) {
-//         return false;
-//     }
-//     for (let i = 2; i < num; i++) {
-//         if (num % i === 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-// console.log(isPrime(7));
+// Using reduce()
+const num = [5, 10, 15, 20];
+const totalSum = num.reduce((sum, num) => sum + num, 0);
+console.log(totalSum);
